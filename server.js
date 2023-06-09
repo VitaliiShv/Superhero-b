c03b86d13766de5553abe0b4e61d27b75c9569b1
Vitalii Shved <vitalyashved@gmail.com>
2023-06-09 15:01:08 +0300

    move DB_HOST to process.env

--- server.js
+++ server.js
@@ -2,12 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Vitalii:S8hDbi4qiO8pFdPn@cluster0.3rbis32.mongodb.net/superheroes-collection?retryWrites=true&w=majority";
-
-//S8hDbi4qiO8pFdPn
-
-// const { DB_HOST } = process.env;
+const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
