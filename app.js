c9b81fd4cb6ebe8159a979f5fb7c2a02aaa805f7
Vitalii Shved <vitalyashved@gmail.com>
2023-06-16 16:10:33 +0300

    fix path

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/heroes", heroRouter);
+app.use("/api/superheroes", heroRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
